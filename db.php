7284e825d44ebcf71a3daa41fd6bf58e8f363dbb
ahmaduae123 <ahmadchatgptwala@gmail.com>
2025-06-03 13:43:04 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$host = 'localhost';
+$dbname = 'dbntq7dcowavgn';
+$username = 'urnrgaote95vf';
+$password = 'tgk9ztof7xb1';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
